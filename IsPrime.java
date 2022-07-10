72c953aa5a2f72db2fd76eb8492bb52849b78d8a
GaneshKumar-0309 <109011186+GaneshKumar-0309@users.noreply.github.com>
2022-07-10 10:18:26 +0530

    Add files via upload

---
+++ IsPrime.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+
+		int n=17, count=0;
+		
+		for (int i = 1; i <=n; i++) {
+			
+		    if (n%i==0) {
+			
+		    	count=count+1;
+		    
+		  }
+		}
+		    
+		    if(count==2) {
+		    	
+		    	System.out.println( " prime number");
+		    }else {
+		    	
+		    	System.out.println( "non prime number");
+		    }
+	}
+ }
